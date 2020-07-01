19e5e19451affd36ad9bb0d9e5a765b5a445bdd3
Imansdn <imansadrian@gmail.com>
2020-07-01 20:36:32 +0430

    refactor: implement new interface in ExploreDbDataImpl.kt

--- app/src/main/java/com/imandroid/simplefoursquare/data/db/ExploreDbDataImpl.kt
+++ app/src/main/java/com/imandroid/simplefoursquare/data/db/ExploreDbDataImpl.kt
@@ -3,17 +3,34 @@ package com.imandroid.simplefoursquare.data.db
 import com.imandroid.simplefoursquare.data.dataSource.ExploreDBDataSource
 import com.imandroid.simplefoursquare.data.db.dao.ExploreDao
 import com.imandroid.simplefoursquare.data.db.table.ExploreEntity
-import io.reactivex.Flowable
 import io.reactivex.Maybe
+import io.reactivex.Single
 
 class ExploreDbDataImpl(private val dao: ExploreDao):ExploreDBDataSource {
 
-    override fun getAllExplores(limit:String ,offset:String): Flowable<List<ExploreEntity>> {
-        return dao.getAllExplores(limit.toInt(),offset.toInt())
+    override fun getAllExplores(limit:String ,pageNumber:String): Maybe<List<ExploreEntity>> {
+         val equivalentOffset = (limit.toInt() * (pageNumber.toInt()+1)) - limit.toInt()
+        return dao.getAllExplores(limit = limit.toInt(), offset = equivalentOffset)
+    }
+
+    override fun getExploreById(explore_id: String): Maybe<ExploreEntity> {
+        return dao.getExploreByID(explore_id=explore_id)
+    }
+
+    override fun updateExplore(exploreEntity: ExploreEntity):Int {
+       return dao.update(exploreEntity)
+    }
+
+    override fun getExplorePrimaryKeyByID(explore_id: String): Single<Int> {
+        return dao.getExplorePrimaryKeyByID(explore_id)
+    }
+
+    override fun getAllExploresCount(): Single<Int> {
+        return dao.getAllExploresCount()
     }
 
     override fun addAllExplores(explores: List<ExploreEntity>) {
-        dao.insertAll(explores)
+        dao.insertAll(explores = explores)
     }
 
     override fun clearAllExplores() {
