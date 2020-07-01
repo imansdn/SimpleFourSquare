0fbe9d834d02d05eb6a1689569c8c4863422c27f
Imansdn <imansadrian@gmail.com>
2020-07-01 20:35:29 +0430

    refactor: make some fields of entities nullable

--- app/src/main/java/com/imandroid/simplefoursquare/data/db/table/ExploreEntity.kt
+++ app/src/main/java/com/imandroid/simplefoursquare/data/db/table/ExploreEntity.kt
@@ -1,15 +1,17 @@
 package com.imandroid.simplefoursquare.data.db.table
 
 import androidx.room.Entity
+import androidx.room.Index
 import androidx.room.PrimaryKey
 import androidx.room.TypeConverters
 import com.imandroid.simplefoursquare.util.Converters
 
 @Entity(tableName = "explores_table")
+//@Entity(tableName = "explores_table",indices =[Index(value = ["explore_id"], unique = true)])
 @TypeConverters(Converters::class)
 data class ExploreEntity(
     @PrimaryKey(autoGenerate = true)
-    val id: Long = 0L,
+    var id: Long = 0L,
     val explore_id: String,
     val name: String?,
     val lat: Double?,
@@ -20,10 +22,10 @@ data class ExploreEntity(
     val country: String?,
     val categories: List<CategoryEntity>,
     val photos: List<String>?,
-    val likes:String="",
-    val rating:String="",
-    val ratingColor:String="",
-    val createdAt:String="",
+    val likes:String?="",
+    val rating:String?="",
+    val ratingColor:String?="",
+    val createdAt:String?="",
     val tips:List<TipEntity> = listOf(),
-    val shortUrl:String = ""
+    val shortUrl:String? = ""
 )
--- app/src/main/java/com/imandroid/simplefoursquare/data/db/table/TipEntity.kt
+++ app/src/main/java/com/imandroid/simplefoursquare/data/db/table/TipEntity.kt
@@ -6,9 +6,9 @@ import androidx.room.PrimaryKey
 @Entity
 data  class TipEntity(
     @PrimaryKey(autoGenerate = true)
-    val id: Long = 0L,
-    val message: String = "",
-    val firstName: String = "",
-    val lastName: String,
-    val userAvatarUrl: String = ""
+    val id: Long? = 0L,
+    val message: String? = "",
+    val firstName: String? = "",
+    val lastName: String?="",
+    val userAvatarUrl: String? = ""
 )
