766c22e0d4d24feb316d32b4ff44cf9bf6afc5bd
Imansdn <imansadrian@gmail.com>
2020-07-01 20:38:13 +0430

    refactor: implement all interfaces of api data source

--- app/src/main/java/com/imandroid/simplefoursquare/data/network/ExploreApiDataImpl.kt
+++ app/src/main/java/com/imandroid/simplefoursquare/data/network/ExploreApiDataImpl.kt
@@ -2,16 +2,22 @@ package com.imandroid.simplefoursquare.data.network
 
 import com.imandroid.simplefoursquare.data.dataSource.ExploreApiDataSource
 import com.imandroid.simplefoursquare.data.network.dto.GetAllExploresDTO
-import io.reactivex.Flowable
+import com.imandroid.simplefoursquare.data.network.dto.GetExploreDetailsDTO
 import io.reactivex.Maybe
+
 
 class ExploreApiDataImpl:ExploreApiDataSource {
     private val api: Api = ApiGenerator.createService(Api::class.java)
 
-    override fun getAllExplores(latlong: String, limit: String, offset: String): Maybe<GetAllExploresDTO> {
-        return api.getAllExploresByLatLng(latlong = latlong,limit = limit ,offset = offset )
+    override fun getAllExplores(latlong: String, limit: String, pageNumber: String): Maybe<GetAllExploresDTO> {
+        val equivalentOffset = (limit.toInt() * (pageNumber.toInt()+1)) - limit.toInt()
+
+        return api.getAllExploresByLatLng(latlong = latlong,limit = limit ,offset = equivalentOffset.toString() )
     }
 
+    override fun getExploreDetails(explore_id: String): Maybe<GetExploreDetailsDTO> {
+        return api.getExploreDetailsById(explore_id=explore_id)
+    }
 
 
 }
