7b0778a27e16c50c507694f6f61098bb1d7748a8
Imansdn <imansadrian@gmail.com>
2020-07-01 20:26:14 +0430

    refactor: add getExploreDetails function to api data source

--- app/src/main/java/com/imandroid/simplefoursquare/data/dataSource/ExploreApiDataSource.kt
+++ app/src/main/java/com/imandroid/simplefoursquare/data/dataSource/ExploreApiDataSource.kt
@@ -1,11 +1,14 @@
 package com.imandroid.simplefoursquare.data.dataSource
 
 import com.imandroid.simplefoursquare.data.network.dto.GetAllExploresDTO
+import com.imandroid.simplefoursquare.data.network.dto.GetExploreDetailsDTO
 import io.reactivex.Flowable
 import io.reactivex.Maybe
 
 interface ExploreApiDataSource {
 
     fun getAllExplores(latlong:String ,limit:String ,offset:String): Maybe<GetAllExploresDTO>
+
+    fun getExploreDetails(explore_id:String): Maybe<GetExploreDetailsDTO>
 
 }
